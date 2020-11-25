4de12904e7fa03f24c20682c8730845960a6aad1
Lia Troy <liatroy@github.com>
2020-11-24 23:53:59 -0500

    Initial code for neural net. Success saving and loading small net with small training corpus.

---
+++ src/test_net.py
@@ -0,0 +1,62 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from train_net import NN_LM
+import pickle
+
+# converts data to correct input type
+def convert2ids(data, word2id):
+	ids = []
+
+	for word in data:
+		id = word2id[word]
+		if id == None:
+			ids.append("unk")
+		else:
+			ids.append(id)
+
+	return ids
+
+def test_model(test_corpus, word2id, id2word):
+    for sentence in test_corpus:
+        data = sentence.split()
+        data_ids = convert2ids(data, word2id)
+        data_ids = torch.LongTensor(data_ids)
+
+        output = model(data_ids)
+        output = output.detach().numpy()
+        
+        next = np.argmax(output)
+
+        print(sentence, ":", id2word[next])
+
+#load test data
+
+test_corpus = [
+'reached for the',
+'the empty roof',
+'force which he',
+'the black empathy',
+'rest of the',
+'thought , and',
+'work vacuity specific',
+'he off to'
+]
+
+model = NN_LM(77)
+model.load_state_dict(torch.load("trained_model.pt"))
+model.eval()
+
+file = open("word2id.pkl", "rb")
+word2id = pickle.load(file)
+file.close()
+
+file = open("id2word.pkl", "rb")
+id2word = pickle.load(file)
+file.close()
+
+test_model(test_corpus, word2id, id2word)
+
+
---
+++ src/train_net.py
@@ -0,0 +1,97 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import pickle
+
+class NN_LM(nn.Module):
+
+	def __init__(self,vocab_size):
+		self.hidden_size = 15
+		self.emb_size = 5
+		super(NN_LM, self).__init__()
+		self.emb = nn.Embedding(vocab_size, self.emb_size)
+		self.H_lyr = nn.Linear(3*self.emb_size, self.hidden_size)
+		self.U_lyr = nn.Linear(self.hidden_size,vocab_size)
+
+	def forward(self,inputs):
+		x = self.emb(inputs).view((-1,3*self.emb_size))
+		hid = torch.tanh(self.H_lyr(x))
+		out = F.log_softmax(self.U_lyr(hid),dim=1)
+		return out
+
+
+def process_training_data(corpus_text):
+    """Tokenizes a text file."""
+    # Create the model's vocabulary and map to unique indices
+    word2id = {}
+    id2word = []
+
+    for word in corpus_text.split():
+        if word not in word2id:
+            id2word.append(word)
+            word2id[word] = len(id2word) - 1
+
+    # Convert string of text into string of IDs in a tensor for input to model
+    input_as_ids = []
+    for word in corpus_text.split():
+        input_as_ids.append(word2id[word])
+    # final_ids = torch.LongTensor(input_as_ids)
+
+
+    return input_as_ids,word2id,id2word
+
+# This function runs the training of the model.
+def run_training(train_data,id2word):
+	num_training_epochs = 50
+
+	## Initialize NNLM
+	nnlm_model = NN_LM(len(id2word))
+	## Define the optimizer as Adam
+	nnlm_optimizer = optim.Adam(nnlm_model.parameters(), lr=.001)
+	## Define the loss function as negative log likelihood loss
+	criterion = nn.NLLLoss()
+
+	# Run training for specified number of epochs
+	print('\nNNLM initializing...\n')
+	for epoch in range(num_training_epochs):
+		# Move through data one word (ID) at a time, extracting a window of three
+		# context words, and a target fourth word for the model to predict
+		for i in range(len(train_data) - 3):
+			input_context = torch.LongTensor(train_data[i:i+3])
+			target_word = torch.LongTensor([train_data[i+3]])
+
+			# Run model on input, get loss, update weights
+			nnlm_optimizer.zero_grad()
+			output = nnlm_model(input_context)
+			loss = criterion(output, target_word)
+			loss.backward()
+			nnlm_optimizer.step()
+
+	return nnlm_model
+
+# need to pull training corpus
+train_corpus = """
+okay, he thought ; i'm off to work . he reached for the doorknob that opened the way out into the unlit hall ,
+then shrank back as he glimpsed the vacuity of the rest of the building . it lay in wait for him, out here ,
+the force which he had felt busily penetrating his specific apartment . god , he thought , and reshut the door .
+he was not ready for the trip up those clanging stairs to the empty roof where he had no animal . the echo of
+himself ascending : the echo of nothing . time to grasp the handles , he said to himself , and crossed the living
+room to the black empathy box .
+"""
+
+train_data,word2id,id2word = process_training_data(train_corpus)
+model = run_training(train_data,id2word)
+
+torch.save(model.state_dict(), "trained_model.pt")
+
+file = open("word2id.pkl", "wb")
+pickle.dump(word2id, file)
+file.close()
+
+file = open("id2word.pkl", "wb")
+pickle.dump(id2word, file)
+file.close()
+
+print("Vocab size: ", len(id2word))
